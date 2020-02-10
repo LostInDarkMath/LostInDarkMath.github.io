6c0270de97c438fd129af017505c9707bbee8dc6
Willi <32729196+LostInDarkMath@users.noreply.github.com>
2020-02-10 15:35:02 +0100

    change website template to a template which is optimized for mobile apps

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # LostInDarkMath.github.io
 Just a test of Github pages.
 View it here: https://lostindarkmath.github.io/web
+
+Templates are from:
+- https://inovatik.com/free-landing-page-html-templates.html
--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,6 +1,6 @@
-/* Template: Aria - Business HTML Landing Page Template
+/* Template: Leno - Mobile App HTML Landing Page Template
    Author: Inovatik
-   Created: Jul 2019
+   Created: Mar 2019
    Description: Custom JS file
 */
 
@@ -89,6 +89,100 @@
     });
 
     
+    /* Image Slider - Swiper */
+    var imageSlider = new Swiper('.image-slider', {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false
+		},
+        loop: false,
+        navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+        spaceBetween: 30,
+        slidesPerView: 5,
+		breakpoints: {
+            // when window is <= 380px
+            380: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window is <= 516px
+            516: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            },
+            // when window is <= 768px
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            // when window is <= 992px
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            },
+            // when window is <= 1200px
+            1200: {
+                slidesPerView: 5,
+                spaceBetween: 30
+            },
+        }
+    });
+
+
+    /* Image Slider - Magnific Popup */
+	$('.popup-link').magnificPopup({
+		removalDelay: 300,
+		type: 'image',
+		callbacks: {
+			beforeOpen: function() {
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure ' + this.st.el.attr('data-effect'));
+			},
+			beforeClose: function() {
+				$('.mfp-figure').addClass('fadeOut');
+			}
+		},
+		gallery:{
+			enabled:true //enable gallery mode
+		}
+    });
+
+    
+    /* Video Lightbox - Magnific Popup */
+    $('.popup-youtube, .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+        iframe: {
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/', 
+                    id: function(url) {        
+                        var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+                        if ( !m || !m[1] ) return null;
+                        return m[1];
+                    },
+                    src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                },
+                vimeo: {
+                    index: 'vimeo.com/', 
+                    id: function(url) {        
+                        var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+                        if ( !m || !m[5] ) return null;
+                        return m[5];
+                    },
+                    src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+                }
+            }
+        }
+    });
+
+
     /* Lightbox - Magnific Popup */
 	$('.popup-with-move-anim').magnificPopup({
 		type: 'inline',
@@ -100,30 +194,7 @@
 		midClick: true,
 		removalDelay: 300,
 		mainClass: 'my-mfp-slide-bottom'
-    });
-    
-
-    /* Filter - Isotope */
-    var $grid = $('.grid').isotope({
-        // options
-        itemSelector: '.element-item',
-        layoutMode: 'fitRows'
-    });
-    
-    // filter items on button click
-    $('.filters-button-group').on( 'click', 'a', function() {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-    
-    // change is-checked class on buttons
-    $('.button-group').each( function( i, buttonGroup ) {
-        var $buttonGroup = $( buttonGroup );
-        $buttonGroup.on( 'click', 'a', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
-        });	
-    });
+	});
     
 
     /* Counter - CountTo */
@@ -167,64 +238,6 @@
 			$(this).removeClass('notEmpty');
 		}
     });
-
-
-    /* Call Me Form */
-    $("#callMeForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            lformError();
-            lsubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            lsubmitForm();
-        }
-    });
-
-    function lsubmitForm() {
-        // initiate variables with form content
-		var name = $("#lname").val();
-		var phone = $("#lphone").val();
-		var email = $("#lemail").val();
-		var select = $("#lselect").val();
-        var terms = $("#lterms").val();
-        
-        $.ajax({
-            type: "POST",
-            url: "php/callmeform-process.php",
-            data: "name=" + name + "&phone=" + phone + "&email=" + email + "&select=" + select + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    lformSuccess();
-                } else {
-                    lformError();
-                    lsubmitMSG(false, text);
-                }
-            }
-        });
-	}
-
-    function lformSuccess() {
-        $("#callMeForm")[0].reset();
-        lsubmitMSG(true, "Request Submitted!");
-        $("input").removeClass('notEmpty'); // resets the field label after submission
-    }
-
-    function lformError() {
-        $("#callMeForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass();
-        });
-	}
-
-    function lsubmitMSG(valid, msg) {
-        if (valid) {
-            var msgClasses = "h3 text-center tada animated";
-        } else {
-            var msgClasses = "h3 text-center";
-        }
-        $("#lmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
 
 
     /* Contact Form */
