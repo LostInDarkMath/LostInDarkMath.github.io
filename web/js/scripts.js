e5e55cc9f462a57d91f94615b9cda50a6ca40249
RobWalt <robwalter96@gmail.com>
2020-04-14 15:53:56 +0200

    added new function to call new php script and edited the old function

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -229,21 +229,68 @@ $('#contactForm').validator().on('submit', function(event) {
   }
 });
 
+// init the validator
+// validator files are included in the download package
+// otherwise download from http://1000hz.github.io/bootstrap-validator
+
+$('#contact-form').validator();
+
+
+// when the form is submitted
+$('#contact-form').on('submit', function(e) {
+  // if the validator does not prevent form submit
+  if (!e.isDefaultPrevented()) {
+    var url = 'contact.php';
+
+    // POST values in the background the the script URL
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: $(this).serialize(),
+      success: function(data) {
+        // data = JSON object that contact.php returns
+        console.log('YES');
+
+        // we recieve the type of the message: success x danger and apply it to
+        // the
+        var messageAlert = 'alert-' + data.type;
+        var messageText = data.message;
+
+        // let's compose Bootstrap alert box HTML
+        var alertBox = '<div class="alert ' + messageAlert +
+            ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+            messageText + '</div>';
+
+        // If we have messageAlert and messageText
+        if (messageAlert && messageText) {
+          // inject the alert to .messages div in our form
+          $('#contact-form').find('.messages').html(alertBox);
+          // empty the form
+          $('#contact-form')[0].reset();
+        }
+      }
+    });
+    console.log('NO');
+    return false;
+  }
+})
+
 function csubmitForm() {
   // initiate variables with form content
   var name = $('#cname').val();
   var email = $('#cemail').val();
   var message = $('#cmessage').val();
   var terms = $('#cterms').val();
-  console.log('bis hier gehts')
-  console.log(name)
+
+  console.log('bis hier gehts');
+  console.log(name);
   $.ajax({
     type: 'POST',
     url: 'php/contactform-process.php',
     data: 'name=' + name + '&email=' + email + '&message=' + message +
         '&terms=' + terms,
-    success: function(text) {
-      if (text == 'success') {
+    success: function(data) {
+      if (data == 'success') {
         cformSuccess();
       } else {
         cformError();
