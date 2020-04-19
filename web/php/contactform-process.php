086af10730839aceeb329142359f1e99cd6d7934
RobWalt <robwalter96@gmail.com>
2020-04-19 11:40:45 +0200

    added working email address to php script

--- web/php/contactform-process.php
+++ web/php/contactform-process.php
@@ -25,8 +25,8 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Leno landing page";
+$EmailTo = "zombie2firend@gmail.com";
+$Subject = "New message from Z2F landing page";
 
 // prepare email body text
 $Body = "";
